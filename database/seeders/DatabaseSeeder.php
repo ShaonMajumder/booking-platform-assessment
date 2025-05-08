d91207522951139d70d03484f380ddd98fb6965f
Shaon Majumder <smazoomder@gmail.com>
2025-05-09 04:43:04 +0600

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,8 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
+            AdminUserSeeder::class,
             ServiceSeeder::class,
-            BookingSeeder::class,
+            BookingSeeder::class
         ]);
     }
 }
